ad88fd5ec703bc233197f74b01812b460fff614d
MYansane2008 <madou.yansane@hgs.hiddengeniusproject.org>
2024-09-13 17:41:44 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,8 @@
-function counter() {
-// Write your code below. Hint: you can finish it in about 3-5 lines.
+function counter() { 
+    counter_number = document.getElementById;    
+    // Write your code below. Hint: you can finish it in about 3-5 lines.
+    //
+    counter_number = document.getElementById('counter-number');
+    counter_number.innerHTML = parseInt(counter_number.innerHTML) +1
+
 }
