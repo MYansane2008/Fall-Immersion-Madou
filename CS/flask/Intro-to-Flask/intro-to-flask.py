6ee872a7026e8f7450165dab9b45df513bb7d8c9
MYansane2008 <madou.yansane@hgs.hiddengeniusproject.org>
2024-12-04 18:13:23 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,9 @@
-# instruction will below. 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Home Page!"
+if __name__ == '__main__':
+    app.run(debug = True)
